21abb4e222e78e7f9fba48e52b6b8190e1638b4c
Aldo Amati <aldilo08@hotmail.com>
2024-08-09 18:13:03 -0300

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
 export function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Test</Button>
+    </div>
   )
 }
 
