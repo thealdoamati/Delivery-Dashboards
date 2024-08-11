54a511ba837e0ef0a0482902d6884099e93d13eb
Aldo Amati <112902097+thealdoamati@users.noreply.github.com>
2024-08-11 20:58:55 -0300

    colors e login

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,9 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export function SignIn() {
   return (
     <>
@@ -10,8 +14,19 @@ export function SignIn() {
             <h1 className="text-2xl font-semibold tracking-tight">
               Acessar painel
             </h1>
-            <p>Acompanhe suas vendas pelo painel do parceiro!</p>
+            <p className="text-xs text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
           </div>
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" />
+            </div>
+            <Button className="w-full" type="submit">
+              Acessar painel
+            </Button>
+          </form>
         </div>
       </div>
     </>
