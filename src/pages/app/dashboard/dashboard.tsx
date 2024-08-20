0ecc7e9e6218365583b74da91ed80f59ced5f2c2
Aldo Amati <112902097+thealdoamati@users.noreply.github.com>
2024-08-20 13:59:29 -0300

    PieChart

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,6 +4,7 @@ import { DayOrdersAmountCard } from './day-orders-amount'
 import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount-card copy'
 import { MonthOrdersAmountCard } from './month-orders-amount-card'
 import { MonthRevenueCard } from './month-revenue-card'
+import { PopularProductsChart } from './popular-chart'
 import { RevenueChart } from './revenue-chart'
 
 export function Dashboard() {
@@ -21,6 +22,7 @@ export function Dashboard() {
 
         <div className="grid grid-cols-9 gap-4">
           <RevenueChart />
+          <PopularProductsChart />
         </div>
       </div>
     </>
--- src/pages/app/dashboard/popular-chart.tsx
+++ src/pages/app/dashboard/popular-chart.tsx
@@ -1,13 +1,5 @@
 import { BarChart } from 'lucide-react'
-import {
-  CartesianGrid,
-  Line,
-  LineChart,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from 'recharts'
+import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts'
 import colors from 'tailwindcss/colors'
 
 import {
@@ -26,6 +18,14 @@ const data = [
   { product: 'Catupiry', amount: 15 },
 ]
 
+const COLORS = [
+  colors.sky['500'],
+  colors.amber['500'],
+  colors.violet['500'],
+  colors.emerald['500'],
+  colors.rose['500'],
+]
+
 export function PopularProductsChart() {
   return (
     <>
@@ -40,28 +40,58 @@ export function PopularProductsChart() {
         </CardHeader>
         <CardContent>
           <ResponsiveContainer width="100%" height={240}>
-            <LineChart data={data} style={{ fontSize: 12 }}>
-              <YAxis
-                stroke="#888"
-                axisLine={false}
-                tickLine={false}
-                width={80}
-                tickFormatter={(value: number) =>
-                  value.toLocaleString('pt-BR', {
-                    style: 'currency',
-                    currency: 'BRL',
-                  })
-                }
-              />
-              <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
-              <CartesianGrid vertical={false} className="stroke-muted" />
-              <Line
-                type="linear"
-                strokeWidth={2}
-                dataKey="revenue"
-                stroke={colors.violet['500']}
-              />
-            </LineChart>
+            <PieChart style={{ fontSize: 12 }}>
+              <Pie
+                data={data}
+                dataKey="amount"
+                name="product"
+                cx="50%"
+                cy="50%"
+                innerRadius={64}
+                outerRadius={86}
+                strokeWidth={8}
+                labelLine={false}
+                label={({
+                  cx,
+                  cy,
+                  midAngle,
+                  innerRadius,
+                  outerRadius,
+                  value,
+                  index,
+                }) => {
+                  const RADIAN = Math.PI / 180
+                  const radius = 12 + innerRadius + (outerRadius - innerRadius)
+                  const x = cx + radius * Math.cos(-midAngle * RADIAN)
+                  const y = cy + radius * Math.sin(-midAngle * RADIAN)
+
+                  return (
+                    <text
+                      x={x}
+                      y={y}
+                      className="fill-muted-foreground text-xs"
+                      textAnchor={x > cx ? 'start' : 'end'}
+                      dominantBaseline="central"
+                    >
+                      {data[index].product.length > 12
+                        ? data[index].product.substring(0, 12).concat('...')
+                        : data[index].product}{' '}
+                      ({value})
+                    </text>
+                  )
+                }}
+              >
+                {data.map((_, index) => {
+                  return (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={COLORS[index]}
+                      className="stroke-background hover:opacity-80"
+                    />
+                  )
+                })}
+              </Pie>
+            </PieChart>
           </ResponsiveContainer>
         </CardContent>
       </Card>
