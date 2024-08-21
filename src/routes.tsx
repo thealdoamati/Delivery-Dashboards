8342069b8c34d1666218508b1d1d0712deb0827c
Aldo Amati <aldilo08@hotmail.com>
2024-08-21 13:31:25 -0300

    404

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,11 +6,13 @@ import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
 import { Dashboard } from './pages/app/dashboard/dashboard'
+import { NotFound } from './pages/404'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement:<NotFound />,
     children: [
       {
         path: '/',
