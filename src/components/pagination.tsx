45a4f8a75ecbbcd8bc4ab0135e2277ca8be36872
Aldo Amati <112902097+thealdoamati@users.noreply.github.com>
2024-09-26 21:05:04 -0300

    upd: Teste paginação

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -24,7 +24,7 @@ export function Pagination({
   const pages = Math.ceil(totalCount / perPage) || 1
   return (
     <div className="flex items-center justify-between">
-      <span className="tex-sm font-medium">Total de {totalCount} items(s)</span>
+      <span className="tex-sm font-medium">Total de {totalCount} item(s)</span>
       <div className="flex items-center gap-6 lg:gap-8">
         <div className="text-sm font-medium">
           Página {pageIndex + 1} de {pages}
